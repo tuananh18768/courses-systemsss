932228b2b37cd72cbfaf976a3afda56287f7be09
bachTuan <bachtuananh16@gmail.com>
2022-04-16 23:59:48 +0700

    alo2

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -296,12 +296,17 @@ const getAllIdeaOfUser = async(req, res) => {
 const downloadZipFile = async(req, res) => {
     try {
         const doc = await ManyFileModel.find();
-        const file = doc
-            .map((e) => e.files)
-            .reduce((acc, currentValue) => acc.concat(currentValue), []);
+        // const loc = doc.reduce(
+        //(acc, currentValue) => acc.concat(currentValue.files), []
+        // );
+        // .map((e) => e.files)
+        // .reduce((acc, currentValue) => acc.concat(currentValue), []);
+        // console.log(loc);
+        const file = doc.reduce(
+            (acc, currentValue) => acc.concat(currentValue.files), []
+        );
         const zip = new admzip();
         file.forEach((fil) => {
-            console.log(fil.filePath);
             zip.addLocalFile(fil.filePath);
         });
         const outputPath = "/" + Date.now() + "output.zip";
